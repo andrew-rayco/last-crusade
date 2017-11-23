1017d260fef4c0cb791fef3281facd91af894495
Andrew <andy@andycondon.com>
2017-11-24 11:25:39 +1300

    69 completed

--- async-foundations/index.js
+++ async-foundations/index.js
@@ -1,9 +1,13 @@
-function greet (name, formatter) {
-  return 'hello, ' + formatter(name)
-}
 
-function formatUpperCase(name) {
-  return name.toUpperCase()
-}
 
-greet('Tim', formatUpperCase)
+function countDown(seconds) {
+  var intervalId = setInterval(function() {
+    seconds--
+    if (seconds > 0) {
+      console.log('Timer:', seconds)
+    } else {
+      console.log('Ring Ring Ring!')
+      clearInterval(intervalId)
+    }
+  }, 1000)
+}
