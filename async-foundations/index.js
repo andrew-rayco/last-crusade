41e9ac668a508651d26cfc8fbb894379fc10976d
Andrew <andy@andycondon.com>
2017-11-24 13:22:37 +1300

    72 completed

--- async-foundations/index.js
+++ async-foundations/index.js
@@ -1,11 +1,23 @@
-function countDown(seconds) {
-  var intervalId = setInterval(function() {
-    seconds--
-    if (seconds > 0) {
-      console.log('Timer:', seconds)
-    } else {
-      console.log('Ring Ring Ring!')
-      clearInterval(intervalId)
-    }
-  }, 1000)
-}
+var newPromise = new Promise((resolve, reject) => {
+  resolve(5)
+})
+
+newPromise.then((data) => {
+  var p = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('first promise:', data)
+      resolve(data * 2)
+    }, 3000)
+  })
+  return p
+}).then((data2) => {
+  var p = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('data2', data2)
+      resolve(data2 + 20)
+    }, 3000)
+  });
+  return p
+}).then((data3) => {
+  console.log(data3)
+})
